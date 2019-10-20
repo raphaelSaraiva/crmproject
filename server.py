e95510121c5826cf0105d0331bc9b3e71cede8dc
raphaelSaraiva <raphael.saraiva13@gmail.com>
2019-10-20 10:40:14 -0300

    adjust

--- server.py
+++ server.py
@@ -1,12 +1,56 @@
 import requests
-from flask import Flask
+import os
+from sessions import RedisSessionInterface
+from datetime import timedelta, datetime, date
+from flask import (Flask, render_template, g, redirect, url_for, request,
+                   session, abort)
 
 
 app = Flask(__name__)
+app_root = os.path.dirname(os.path.abspath(__file__))
+app.session_interface = RedisSessionInterface()
+app.secret_key = os.urandom(24)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    if(not g.user):
+        return redirect(url_for('login'))
+
+    return render_template('busca.html')
 
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if(g.user):
+        return abort(404)
+
+    if(request.method == 'POST'):
+        session.pop('user', None)
+        req = request.form
+        if(True):
+            session.permanent = True
+            session['user'] = req['username']
+            return redirect(url_for('cadastro'))
+        else:
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/cadastro')
+def pontos_cadastro():
+    if(not g.user):
+        return redirect(url_for('login'))
+
+    return render_template('cadastro.html')
+
+
+@app.route('/busca')
+def pontos_cadastro():
+    if(not g.user):
+        return redirect(url_for('login'))
+
+    return render_template('cadastro.html')
